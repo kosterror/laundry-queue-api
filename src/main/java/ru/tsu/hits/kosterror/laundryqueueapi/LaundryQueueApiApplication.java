1387c21c7d56ab6727344a9af955b96ad9a9f547
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-06-10 02:41:58 +0700

    Initial of project.

---
+++ src/main/java/ru/tsu/hits/kosterror/laundryqueueapi/LaundryQueueApiApplication.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.kosterror.laundryqueueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryQueueApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaundryQueueApiApplication.class, args);
+    }
+
+}
